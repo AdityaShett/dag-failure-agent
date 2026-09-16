0e9c4b489db128fbf0a7ff96d5e9fb79f7930052
AdityaShett <adityashetty559@gmail.com>
2026-09-16 09:10:34 -0400

    Agent-proposed fix for run_batch_job failure (run-ambiguous_log_dag6-20260916T035813Z-ca50343f)

--- tests/dag6.py
+++ tests/dag6.py
@@ -16,7 +16,7 @@ def store_result(**context):
     print("Storing reconciliation result")
 
 with DAG("dag6", start_date=datetime(2026, 1, 1), schedule=None, catchup=False) as dag:
-
+    run_batch_job = PythonOperator(task_id="run_batch_job", python_callable=lambda: print("Executing run_batch_job placeholder"))
 
 # Agent RCA Test
 # DAG: dag6
